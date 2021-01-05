527011721840b684f64825e0ac66910a59cf45ab
Siddhesh Agarwal <68057995+Siddhesh-Agarwal@users.noreply.github.com>
2021-01-05 18:36:13 +0530

    Update ToDoList.py

--- TODO List/ToDoList.py
+++ TODO List/ToDoList.py
@@ -4,9 +4,6 @@ import tkinter.messagebox
 import pickle
 
 def App():
-    root = tkinter.Tk()
-    root.title("To-Do List")
-    
     # Adding Task
     def add_task():
         task = entry_task.get()
@@ -15,7 +12,7 @@ def App():
             entry_task.delete(0, tkinter.END)
         else:
             tkinter.messagebox.showwarning("Warning!", "You must enter a task")
- 
+
     # Deleting Task
     def delete_task():
         try:
@@ -41,19 +38,36 @@ def App():
             pickle.dump(tasks, open("tasks.dat", "wb"))
         except:
             pass
-
-    frame_tasks = tkinter.Frame(root).pack()
-    listbox_tasks = tkinter.Listbox(frame_tasks, height=10, width=50).pack(side=tkinter.LEFT)
-    scrollbar_tasks = tkinter.Scrollbar(frame_tasks).pack(side=tkinter.RIGHT, fill=tkinter.Y)
+    
+    # GUI
+    root = tkinter.Tk()
+    root.title("To-Do List")
+    frame_tasks = tkinter.Frame(root)
+    frame_tasks.pack()
+    
+    listbox_tasks = tkinter.Listbox(frame_tasks, height=10, width=50)
+    listbox_tasks.pack(side=tkinter.LEFT)
+    
+    scrollbar_tasks = tkinter.Scrollbar(frame_tasks)
+    scrollbar_tasks.pack(side=tkinter.RIGHT, fill=tkinter.Y)
 
     listbox_tasks.configure(yscrollcommand=scrollbar_tasks.set)
     scrollbar_tasks.configure(command=listbox_tasks.yview)
 
-    entry_task = tkinter.Entry(root, width=50).pack()
-    button_add_task = tkinter.Button(root, text="Add task", width=48, command=add_task).pack()
-    button_delete_task = tkinter.Button(root, text="Delete task", width=48, command=delete_task).pack()
-    button_load_tasks = tkinter.Button(root, text="Load tasks", width=48, command=load_tasks).pack()
-    button_save_tasks = tkinter.Button(root, text="Save tasks", width=48, command=save_tasks).pack()
+    entry_task = tkinter.Entry(root, width=50)
+    entry_task.pack()
+    
+    button_add_task = tkinter.Button(root, text="Add task", width=48, command=add_task)
+    button_add_task.pack()
+    
+    button_delete_task = tkinter.Button(root, text="Delete task", width=48, command=delete_task)
+    button_delete_task.pack()
+    
+    button_load_tasks = tkinter.Button(root, text="Load tasks", width=48, command=load_tasks)
+    button_load_tasks.pack()
+    
+    button_save_tasks = tkinter.Button(root, text="Save tasks", width=48, command=save_tasks)
+    button_save_tasks.pack()
 
     root.mainloop()
 
